ce9560f6151ac58febf29e22de5e428def3e64df
AdamGrennan <adamgr5000@gmail.com>
2025-03-28 16:21:22 +0000

    Initial Commit

---
+++ src/main/java/com/example/demo/SoftwarepatternsCa4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarepatternsCa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarepatternsCa4Application.class, args);
+	}
+
+}
