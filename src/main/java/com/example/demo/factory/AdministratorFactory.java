bff19b7854cf417966a9ef796b89034c4f5b776e
AdamGrennan <adamgr5000@gmail.com>
2025-04-15 16:22:38 +0100

    Removed unecesary user builder just used factory

--- src/main/java/com/example/demo/factory/AdministratorFactory.java
+++ src/main/java/com/example/demo/factory/AdministratorFactory.java
@@ -2,16 +2,18 @@ package com.example.demo.factory;
 
 import com.example.demo.model.Role;
 import com.example.demo.model.User;
-import com.example.demo.builder.UserDirector;
-import com.example.demo.builder.UserBuilder;
-import com.example.demo.builder.ConcreteUserBuilder;
 
 public class AdministratorFactory extends Factory {
     @Override
     public User createUser(String username, String email, String password,
                            String address, String paymentMethod) {
-        UserBuilder builder = new ConcreteUserBuilder(username, email, Role.ADMINISTRATOR, password, address, paymentMethod);
-        UserDirector director = new UserDirector(builder);
-        return director.constructUser();
+    	 User user = new User();
+         user.setUsername(username);
+         user.setEmail(email);
+         user.setPassword(password);
+         user.setAddress(address);
+         user.setPaymentMethod(paymentMethod);
+         user.setRole(Role.ADMINISTRATOR);
+         return user;
     }
 }
--- src/main/java/com/example/demo/factory/CustomerFactory.java
+++ src/main/java/com/example/demo/factory/CustomerFactory.java
@@ -1,8 +1,5 @@
 package com.example.demo.factory;
 
-import com.example.demo.builder.UserDirector;
-import com.example.demo.builder.UserBuilder;
-import com.example.demo.builder.ConcreteUserBuilder;
 import com.example.demo.model.Role;
 import com.example.demo.model.User;
 
@@ -10,8 +7,13 @@ public class CustomerFactory extends Factory {
     @Override
     public User createUser(String username, String email, String password,
                            String address, String paymentMethod) {
-        UserBuilder builder = new ConcreteUserBuilder(username, email, Role.CUSTOMER, password, address, paymentMethod);
-        UserDirector director = new UserDirector(builder);
-        return director.constructUser(); 
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setAddress(address);
+        user.setPaymentMethod(paymentMethod);
+        user.setRole(Role.CUSTOMER);
+        return user;
     }
 }
